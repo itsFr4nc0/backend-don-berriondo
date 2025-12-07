9b8b91b6acbdffc2cd0bfcd481fb5cf90ed07182
itsFr4nc0 <alexius9753@gmail.com>
2025-12-07 10:11:01 -0500

    Manejo de stock

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getProducts, getProductById } from "../controllers/productsController.js";
+import { getProducts, getProductById, updateStock } from "../controllers/productsController.js";
 const router = express.Router();
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
+router.post("/update-stock", updateStock);
 
 export default router;
